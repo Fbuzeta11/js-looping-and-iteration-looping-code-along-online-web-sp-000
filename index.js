16cdeaff8f8d252dfcef80d503b53c5b03500f2d
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2021-05-03 17:10:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const cards = (["Lisa", "Kaitlin", "Jan"]);
+ 
+function writeCards(cards, event) {
+  let result = []
+  for (let i = 0; i < cards.length; i++) {
+    result.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  }
+  return result
+}
+
+function countDown() {
+  var i = 0;
+for (; i < 9; i++) {
+    console.log(i);
+    // more statements
+      
+      countDown(10);
+    }
+    
+    
+}
+ 
+
+
